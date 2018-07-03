1481f82e7de0250c92f983b442551c41638fb942
diweihua <weihua@teambition.com>
2018-07-03 14:04:59 +0800

    add ssl for envoy

--- echo_envoy_sidecar/cmd.sh
+++ echo_envoy_sidecar/cmd.sh
@@ -13,3 +13,11 @@ kubectl create -f echo_envoy_svc.yaml
 
 # show pods
 kubectl get pod -n envoy-test -o wide
+
+# https://blog.turbinelabs.io/setting-up-ssl-with-envoy-f7c5aa06a5ce
+# gen ssl cert
+openssl req -x509 -newkey rsa:4096 -keyout example-com.key -out example-com.crt -days 365
+openssl rsa -in example-com.key -out example-com.key.unsecure
+
+sudo /usr/local/bin/envoy-static -c echo_envoy.yaml -l debug
+$ curl -v https://127.0.0.1:443/ -k -H 'Host: example.com'
